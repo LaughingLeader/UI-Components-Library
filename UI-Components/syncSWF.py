a86a3a14230cd3002316a5779f669375cac1b140
Shresht7 <Shresht7@users.noreply.github.com>
2020-11-21 19:49:50 +0530

    Update syncSWF.py 🐍

--- UI-Components/syncSWF.py
+++ UI-Components/syncSWF.py
@@ -10,13 +10,12 @@ import shutil
 #       DIRECTORIES
 #   ###################
 
-sourceDir = "\\UI-Components\\"
-targetDir = "\\Public\\S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4\\GUI\\"
+#   SOURCE DIRECTORY
+sourceDir = os.getcwd() + "\\UI-Components\\"
 
-currentWorkingDir = os.getcwd()
-
-sourceDir = currentWorkingDir + sourceDir
-targetDir = currentWorkingDir + targetDir
+#   TARGET DIRECTORY
+targetDir = os.getcwd() + \
+    "\\Public\\S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4\\GUI\\"
 
 #   #################
 #       SYNC SWFS
@@ -28,4 +27,7 @@ for subdirectory, directory, files in os.walk(sourceDir):
 
         if filePath.endswith(".swf"):
             shutil.copy(filePath, os.path.join(targetDir + fileName))
-            print(f"{fileName}\nMoved swf at {sourceDir + fileName}\n to {targetDir + fileName}\n")
+
+            dashes = "-" * len(fileName)
+            print(
+                f"{fileName}\n{dashes}\nMoved swf at {sourceDir + fileName}\n to {targetDir + fileName}\n")
