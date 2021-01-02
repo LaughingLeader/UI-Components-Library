1e81e38f2fb2b70808b95fc721b75dc50d8abccc
Shresht7 <Shresht7@users.noreply.github.com>
2021-01-03 00:42:23 +0530

    Sample for ContextMenu

--- Mods/S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4/Story/RawFiles/Lua/Client/Development/DevMode.lua
+++ Mods/S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4/Story/RawFiles/Lua/Client/Development/DevMode.lua
@@ -32,3 +32,30 @@ function RegisterDebugHooks(UI)
         end
     end)
 end
+
+--  =================
+--  TEST CONTEXT MENU
+--  =================
+
+-- ContextMenu:Register({
+--     ["RootTemplate::df7a8779-f908-43ac-b0ba-cb49d16308a9"] = {
+--         {
+--             -- ["ID"] = 1,
+--             ["actionID"] = 271,
+--             ['clickSound'] = true,
+--             ['text'] = "<font color='#3333CC'>Foo</font>",
+--             ['isDisabled'] = false,
+--             ['isLegal'] = true,
+--             ['action'] = function() Ext.Print("Foo") end
+--         },
+--         {
+--         -- ["ID"] = 2,
+--         ["actionID"] = 272,
+--         ['clickSound'] = true,
+--         ['text'] = "<font color='#CC3333'>Bar</font>",
+--         ['isDisabled'] = false,
+--         ['isLegal'] = true,
+--         ['action'] = function() Ext.Print("Bar") end
+--         }
+--      }
+-- })
