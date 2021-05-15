36fbe025fabc2ec5d56a658c58e945dfa7f27538
Shresht7 <Shresht7@users.noreply.github.com>
2021-05-15 15:34:22 +0530

    Update AuxFunctions

--- Mods/S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4/Story/RawFiles/Lua/AuxFunctions/Client/Networker.lua
+++ Mods/S7_UI_Components_Library_b66d56c6-12f9-4abc-844f-0c30b89d32e4/Story/RawFiles/Lua/AuxFunctions/Client/Networker.lua
@@ -8,9 +8,9 @@ Ext.RegisterNetListener('S7_AuxFunctions::UserInfoSync', function (channel, payl
     UserInformation = Integrate(UserInformation, package.UserSpec)
 end)
 
-Ext.RegisterListener('AfterUIInvoke', function(ui, call, handle)
+Ext.RegisterListener('AfterUIInvoke', function(ui, call, double)
     if call ~= 'setPlayerHandle' or call ~= 'selectPlayer' then return end
-    local character = Ext.GetCharacter(handle)
+    local character = Ext.GetCharacter(Ext.DoubleToHandle(double))
     if not character then return end
     UserInformation.CurrentCharacter = character.MyGuid
 end)
